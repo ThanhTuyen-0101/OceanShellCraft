4c1f1282f41cab66b388f830bb7a489a24db2df6
Norah007 <dangngoctamnhu2020@gmail.com>
2026-02-04 15:27:46 +0700

    thu check thay doi

--- obj/Debug/net10.0/OceanShellCraft.AssemblyInfo.cs
+++ obj/Debug/net10.0/OceanShellCraft.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OceanShellCraft")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2c67f8122324135aacb4bfdac05578869c81bcd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e39dc8fec81a762e198c87318c6834c46ef5fd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("OceanShellCraft")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OceanShellCraft")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
